e2d7b759673805573220452fde0c3f48cdce820f
Howard Tang <tangh@google.com>
2013-11-06 13:33:09 +0000

    Changes order of rc and moves session to own folder.

--- recompute/recompute.js
+++ recompute/recompute.js
@@ -1,70 +1,11 @@
 // Examining the deps.js and session module from Meteor.
 var currentComputation = null;
 var nextId = 0;
-// var toFlush = [];
-
-// var requireFlush = function() {
-// 	setTimeout(flush, 0);
-// };
-
-// var flush = function() {
-// 	for (var i = 0; i < toFlush.length; i++) {
-// 		toFlush[i].compute();
-// 	}
-// 	toFlush = [];
-// };
+var toFlush = [];
 
 var contains = function(array, obj) {
 	for (var i = 0; i < array.length; i++) {
 		if (array[i] === obj) return true;
-	}
-};
-
-var Session = {
-	keys: {},
-	keyDeps: {}
-};
-
-Session.get = function(key) {
-
-};
-
-Session.ensureDep = function(key) {
-	// Ensure dependency exists
-	if (!this.keyDeps[key]) {
-		this.keyDeps[key] = new Dependency();
-	}
-};
-
-Session.set = function(key, value) {
-};
-
-var Computation = function(f) {
-	this.id = nextId++;
-	this.func = f;
-};
-
-Computation.prototype = {
-	compute: function() {
-		// sets current computation and runs function.
-	},
-	invalidate: function() {
-		// Adds this to flush list. Calls requireflush and runs any callbacks required.
-	}
-};
-
-// Tracks dependant functions and invalidates upon change.
-var Dependency = function() {
-	this.dependents = {};
-};
-
-Dependency.prototype = {
-	depend: function() {
-		// Registers the current computation as a dependant.
-		// Adds callback to remove from dependants on invalidation.
-	},
-	changed: function() {
-		// Invalidates all dependants.
 	}
 };
 
@@ -74,6 +15,67 @@ autorun = function(f) {
 	c.compute();
 };
 
-autorun(function() {
-	doSomething(session.get('number'));
-})
+// Keeps reference to a function.
+var Computation = function(f) {
+	this.id = nextId++;
+	this.onInvalidateCallbacks = [];
+	this.func = f;
+};
+
+// While running the function, ensure that the current computation is this.
+// After finished running, remove reference to this computation.
+Computation.prototype.compute = function() {
+	currentComputation = this;
+	this.func();
+	currentComputation = null;
+};
+
+// Adds to flush list.
+Computation.prototype.invalidate = function() {
+	if (!contains(toFlush, this)) {
+		toFlush.push(this);
+	}
+	requireFlush();
+	for (var i = 0; i < this.onInvalidateCallbacks; i++) {
+		this.onInvalidateCallbacks[i]();
+	}
+	this.onInvalidateCallbacks = [];
+};
+
+// Tracks dependant functions and invalidates upon change.
+var Dependency = function() {
+	this.dependents = {};
+};
+
+// Adds current computation to list of dependants of this dependency.
+Dependency.prototype.depend = function() {
+	var self = this;
+
+	if (computation = currentComputation) {
+		computationId = computation.id;
+		this.dependents[computationId] = computation;
+		computation.onInvalidateCallbacks.push(function() {
+			delete self.dependents[computationId];
+		});
+	}
+};
+
+Dependency.prototype.changed = function() {
+	for (dep in this.dependents) {
+		this.dependents[dep].invalidate();
+	}
+};
+
+// Computes all required computations on next tick.
+var requireFlush = function() {
+	setTimeout(flush, 0);
+};
+
+var flush = function() {
+	for (var i = 0; i < toFlush.length; i++) {
+		toFlush[i].compute();
+	}
+	toFlush = [];
+};
+
+
